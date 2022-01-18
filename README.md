ea0fa85256aa885865870a8f67b8d4c2b46e2556
delphine nyaboke <delphine@sendyit.com>
2022-01-18 11:35:32 +0300

    add all files from previous courseApi project

---
+++ README.md
@@ -0,0 +1,5 @@
+## description
+this project is meant to give someone a quicktart
+in Spring Boot Application.
+
+i am using [Java Brains](https://www.youtube.com/watch?v=msXL2oDexqw) tutorials.
--- src/main/java/com/delphine/springbootquickstart/courseapidata/CourseApiDataApplication.java
+++ src/main/java/com/delphine/springbootquickstart/courseapidata/CourseApiDataApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseApiDataApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CourseApiDataApplication.class, args);
 	}
 
